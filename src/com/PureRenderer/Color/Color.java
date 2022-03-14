16ff9fe435835763c7db5df5803edb6edd87b79d
Eshan Chatterjee <eshan2001221@gmail.com>
2022-03-14 16:07:52 +0530

    final commit

---
+++ src/com/PureRenderer/Color/Color.java
@@ -0,0 +1,17 @@
+package com.PureRenderer.Color;
+
+public class Color
+{
+    public static java.awt.Color getShade(java.awt.Color color, double shade)
+    {
+        double redLinear = Math.pow(color.getRed(), 2.4) * shade;
+        double greenLinear = Math.pow(color.getGreen(), 2.4) * shade;
+        double blueLinear = Math.pow(color.getBlue(), 2.4) * shade;
+
+        int red = (int) Math.pow(redLinear, 1/2.4);
+        int green = (int) Math.pow(greenLinear, 1/2.4);
+        int blue = (int) Math.pow(blueLinear, 1/2.4);
+
+        return new java.awt.Color(red, green, blue);
+    }
+}
